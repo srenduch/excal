643f43db932e7499faa4218d64816740f339e771
srenduch <73308999+srenduch@users.noreply.github.com>
2022-08-10 00:11:21 -0700

    Update init.py

--- db/init.py
+++ db/init.py
@@ -1,8 +1,8 @@
 import sqlite3
 
-conn = sqlite3.connect('../db/db.db')
+conn = sqlite3.connect('db/db.db')
 
-with open('../db/schema.sql') as f:
+with open('db/schema.sql') as f:
     conn.executescript(f.read())
 
 conn.close()
